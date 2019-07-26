f033753cfeb86aba4c92a5e086bdd0dcb6d10dc5
Jehiel Martinez <jehielmartinez@gmail.com>
2019-07-26 12:12:56 -0600

    DutyDetail added to Router

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -3,9 +3,11 @@ import {createDrawerNavigator, createAppContainer} from 'react-navigation';
 import Dashboard from '../Dashboard/Dashboard';
 import NewList from '../NewList/NewList';
 import SideBar from './SideBar';
+import DutyDetail from '../NewList/DutyDetail';
 
 const drawerNavigation = createDrawerNavigator(
     {
+        DutyDetail: {screen: DutyDetail},
         Dashboard: {screen: Dashboard},
         NewList: {screen: NewList}
     },
