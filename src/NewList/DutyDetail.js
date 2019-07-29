4f86a2e33b654aeafeaa4a0314568c30d86d5130
Jehiel Martinez <jehielmartinez@gmail.com>
2019-07-28 21:40:33 -0600

    customization Header

--- src/NewList/DutyDetail.js
+++ src/NewList/DutyDetail.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, Container, Header, Content, Form, Item, Label, Input, Textarea, Picker, DatePicker, Button } from 'native-base';
+import { Text, Container, Header, Content, Form, Item, Label, Input, Textarea, Picker, DatePicker, Button, Body, Title, Left, Icon } from 'native-base';
 import {StyleSheet} from 'react-native';
 
 export default class DutyDetail extends Component {
@@ -7,32 +7,41 @@ export default class DutyDetail extends Component {
   render() {
     return (
       <Container>
-        <Header/>
+        <Header>
+          <Left>
+            <Button transparent onPress={() => this.props.navigation.goBack()}>
+              <Icon name='arrow-back'/>
+            </Button>
+          </Left>
+          <Body>
+            <Title>New Duty</Title>
+          </Body>
+        </Header>
         <Content padder>
             <Form>
                 <Item bordered regular style={styles.inputStyle}> 
-                  <Input placeholder='Descripción'/>
+                  <Input placeholder='Description'/>
                 </Item>
               
                 <Item bordered regular style={styles.inputStyle}>
-                  <Input placeholder='Monto' keyboardType='numeric'/>
+                  <Input placeholder='Amount' keyboardType='numeric'/>
                 </Item>
                
                 <Item bordered regular style={styles.inputStyle}>
-                    <Textarea placeholder='Notas' rowSpan={5}/>
+                    <Textarea placeholder='Notes' rowSpan={5}/>
                 </Item>
 
                 <Item bordered regular picker style={styles.inputStyle}>
                     <Picker
                         mode='dropdown'
-                        placeholder='Pagar en'
+                        placeholder='Paying Quarter'
                     >
-                        <Picker.Item label='Primera Quincena' value='firstQ'/>
-                        <Picker.Item label='Segunda Quincena' value='lastQ'/>
+                        <Picker.Item label='First Quarter' value='firstQ'/>
+                        <Picker.Item label='Second Quarter' value='lastQ'/>
                     </Picker>
                 </Item>
-                <Button style={styles.buttonStyle} block primary><Text>Almacenar</Text></Button>
-                <Button style={styles.buttonStyle} block danger><Text>Cancelar</Text></Button>
+                <Button style={styles.buttonStyle} block primary><Text>Save</Text></Button>
+                <Button style={styles.buttonStyle} block danger><Text>Cancel</Text></Button>
             </Form>
         </Content>
       </Container>
