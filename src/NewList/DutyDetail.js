f427b0fb0b2b829f5460c43e304fedde43da23f4
Jehiel Martinez <jehielmartinez@gmail.com>
2019-08-01 04:17:16 -0600

    added function to save and cancel buttons

--- src/NewList/DutyDetail.js
+++ src/NewList/DutyDetail.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
 import { Text, Container, Header, Content, Form, Item, Label, Input, Textarea, Picker, DatePicker, Button, Body, Title, Left, Icon } from 'native-base';
 import {StyleSheet} from 'react-native';
+import moment from 'moment'
+import { newDuty } from '../functions/dutyFunctions';
 
 export default class DutyDetail extends Component {
+  state={
+    description: '',
+    amount: '',
+    notes: '',
+    monthHalf: 'firstH'
+  }
+
+  saveDuty = async() => {
+    const {description, amount, notes, monthHalf} = this.state
+    const currentMonth = moment().format('MM')
+
+    const duty = {
+      description,
+      amount,
+      notes,
+      monthHalf,
+      month: currentMonth
+    }
+
+    const response = await newDuty(duty)
+    console.log(response)
+
+    this.props.navigation.goBack()
+  }
   
   render() {
     return (
@@ -20,28 +46,60 @@ export default class DutyDetail extends Component {
         <Content padder>
             <Form>
                 <Item bordered regular style={styles.inputStyle}> 
-                  <Input placeholder='Description'/>
+                  <Input 
+                    placeholder='Description'
+                    value={this.state.description}
+                    onChangeText={(text) => this.setState({description: text})}
+                  />
                 </Item>
               
                 <Item bordered regular style={styles.inputStyle}>
-                  <Input placeholder='Amount' keyboardType='numeric'/>
+                  <Input 
+                    placeholder='Amount' 
+                    keyboardType='numeric'
+                    value={this.state.amount}
+                    onChangeText={(text) => this.setState({amount: text})}
+                  />
                 </Item>
                
                 <Item bordered regular style={styles.inputStyle}>
-                    <Textarea placeholder='Notes' rowSpan={5}/>
+                    <Textarea 
+                      placeholder='Notes' 
+                      rowSpan={5}
+                      value={this.state.notes}
+                      onChangeText={(text) => this.setState({notes: text})}
+                    />
                 </Item>
 
                 <Item bordered regular picker style={styles.inputStyle}>
                     <Picker
                         mode='dropdown'
                         placeholder='Paying Quarter'
+                        onValueChange={(value) => this.setState({monthHalf: value})}
+                        selectedValue={this.state.monthHalf}
                     >
-                        <Picker.Item label='First Quarter' value='firstQ'/>
-                        <Picker.Item label='Second Quarter' value='lastQ'/>
+                        <Picker.Item label='First Half' value='firstH'/>
+                        <Picker.Item label='Second Half' value='lastH'/>
                     </Picker>
                 </Item>
-                <Button style={styles.buttonStyle} block primary><Text>Save</Text></Button>
-                <Button style={styles.buttonStyle} block danger><Text>Cancel</Text></Button>
+
+                <Button 
+                  style={styles.buttonStyle} 
+                  block 
+                  primary
+                  onPress={this.saveDuty}
+                >
+                  <Text>Save</Text>
+                </Button>
+
+                <Button 
+                  style={styles.buttonStyle} 
+                  block 
+                  danger
+                  onPress={() => this.props.navigation.goBack()}
+                >
+                  <Text>Cancel</Text>
+                </Button>
             </Form>
         </Content>
       </Container>
