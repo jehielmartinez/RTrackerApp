8423a2354ca0407cd674da015e0a94cd670aa94f
Jehiel Martinez <jehielmartinez@gmail.com>
2019-08-04 00:44:04 -0600

    delete monthList

--- src/Router/SideBar.js
+++ src/Router/SideBar.js
@@ -4,26 +4,12 @@ import { Container, List, ListItem } from 'native-base';
 import moment from 'moment'
 
 export default class SideBar extends Component {
-  _renderRow = (item) => {
-    return (
-      <ListItem button onPress={() => console.log(item)}>
-        <Text>{moment(item, 'MM').format('MMMM')}</Text>
-      </ListItem>
-    )
-  }
+  
   render() {
-    const months=['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
     return (
       <Container style={styles.container}>
         <View style={styles.brandBar}>
           <Text style={styles.brandText}>Rtracker App v1.0</Text>
-        </View>
-        <View style={styles.monthsList}>
-          <List 
-            scrollEnabled 
-            dataArray={months}
-            renderRow={this._renderRow}>
-          </List>
         </View>
       </Container>
     );
@@ -46,8 +32,5 @@ const styles = StyleSheet.create({
     color: 'white',
     padding: 10,
     fontWeight: 'bold'
-  },
-  monthsList:{
-    maxHeight: 400
   }
 });
